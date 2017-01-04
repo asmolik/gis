641a8c893d7f6c3933b6f0cc8a58ae1312305f32
asmolik <asmolik@users.noreply.github.com>
2017-01-04 19:10:23 +0100

    formatowanie

--- gis/Source/main.cpp
+++ gis/Source/main.cpp
@@ -1,3 +1,4 @@
+﻿#include <ctime>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -13,7 +14,7 @@ void printOutput(const std::string& fileName, const std::vector<Path>& paths, fl
         std::cerr << "output file" << std::endl;
         return;
     }
-    file << "Time elapsed: "<< time << " ms" << std::endl;
+    file << "Time elapsed: " << time << " ms" << std::endl;
     int totalFlow = 0;
     for (auto& p : paths)
     {
@@ -36,15 +37,15 @@ int main(int argc, char* argv[])
     {
         std::cout << " Poprawny sposób uruchomienia programu: " << std::endl;
         std::cout << " ./main plik_z_grafem.txt plik_do_zapisu_sciezek.txt wersja_algorytmu" << std::endl;
-        std::cout << " Gdzie wersja_algorytmu 1 to algorytm SPE, 2 to algorytm LPE" << std::endl<<std::endl;
-        input = "../Test_graphs/graf.txt";
+        std::cout << " Gdzie wersja_algorytmu 1 to algorytm SPE, 2 to algorytm LPE" << std::endl << std::endl;
+        input = "Test_graphs/graf.txt";
         output = "out.txt";
         Graph graph(input);
         const clock_t begin_time = clock();
         auto paths = graph.find_shortest_paths();
-        auto time = float( clock () - begin_time ) /  CLOCKS_PER_SEC;
-        std::cout<<"Wynik dla grafu przykładowego z pliku: \""<<input<<"\" zapisano w pliku \""<<output<<"\""<<std::endl;
-        printOutput(output, paths, time);    
+        auto time = float(clock() - begin_time) / CLOCKS_PER_SEC;
+        std::cout << "Wynik dla grafu przykładowego z pliku: \"" << input << "\" zapisano w pliku \"" << output << "\"" << std::endl;
+        printOutput(output, paths, time);
     }
     else
     {
@@ -52,23 +53,24 @@ int main(int argc, char* argv[])
         output = argv[2];
         mode = argv[3];
         Graph graph(input);
-        if(mode=="2")
+        if (mode == "2")
         {
             const clock_t begin_time = clock();
             auto paths = graph.find_shortest_paths_2();
-            auto time = float( clock () - begin_time ) /  CLOCKS_PER_SEC;
-            std::cout<<"Wynik dla grafu użytkownika z pliku: \""<<input<<"\" zapisano w pliku \""<<output<<"\""<<std::endl;
-            printOutput(output, paths, time);    
+            auto time = float(clock() - begin_time) / CLOCKS_PER_SEC;
+            std::cout << "Wynik dla grafu użytkownika z pliku: \"" << input << "\" zapisano w pliku \"" << output << "\"" << std::endl;
+            printOutput(output, paths, time);
         }
         else
         {
             const clock_t begin_time = clock();
             auto paths = graph.find_shortest_paths();
-            auto time = float( clock () - begin_time ) /  CLOCKS_PER_SEC;
-            std::cout<<"Wynik dla grafu użytkownika z pliku: \""<<input<<"\" zapisano w pliku \""<<output<<"\""<<std::endl;
-            printOutput(output, paths, time);    
- 
+            auto time = float(clock() - begin_time) / CLOCKS_PER_SEC;
+            std::cout << "Wynik dla grafu użytkownika z pliku: \"" << input << "\" zapisano w pliku \"" << output << "\"" << std::endl;
+            printOutput(output, paths, time);
+
         }
- 
+
     }
+    return 0;
 }
